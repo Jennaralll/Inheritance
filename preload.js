d90e3379b3eab06e1c4da513e2ce6ab51b9a122b
oNyCe <missionbit@Missions-MacBook-Pro-2.local>
2017-04-19 16:33:58 -0700

    changes

--- preload.js
+++ preload.js
@@ -1,5 +1,14 @@
-function preload(){
-    game.load.image("bot1", "bot1.jpg", 300, 300);
-    game.load.image("top1", "bg2.png", 300, 300);
-    game.load.spritesheet("princess", "princess.png", 32, 48);
+var preload = function(game){}
+
+preload.prototype= {
+    function preload(){
+        var loadingBg = this.add.sprite(0, 0, "loading_bar.png");
+
+        game.load.image("bot1", "bot1.jpg", 300, 300);
+        game.load.image("top1", "bg2.png", 300, 300);
+        game.load.spritesheet("princess", "princess.png", 32, 48);
+    },
+    create: function(){
+        this.game.state.start("GameTitle");
+    }
 }
